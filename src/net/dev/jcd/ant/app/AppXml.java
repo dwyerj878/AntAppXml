edf034be5e81d470efcd7f84a52c51e32a59a997
dwyerj878 <jcdwyer@jcd-dev.net>
2011-12-02 17:37:41 -0600

    Externalize strings

--- src/net/dev/jcd/ant/app/AppXml.java
+++ src/net/dev/jcd/ant/app/AppXml.java
@@ -19,45 +19,13 @@ import org.apache.tools.ant.types.resources.FileResource;
  * 
  */
 public class AppXml extends Task {
-	private static final String APPLICATION_END = "</application>";
-	private static final String APPLICATION_START_J2EE_5_HEADER = "<application xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-	        + "\n\t\txmlns=\"http://java.sun.com/xml/ns/javaee\" "
-	        + "\n\t\txmlns:application=\"http://java.sun.com/xml/ns/javaee/application_5.xsd\" "
-	        + "\n\t\txsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/application_5.xsd\" "
-	        + "\n\t\tversion=\"5\">";
-
-	private static final String APPLICATION_START_J2EE_6_HEADER = "<application xmlns=\"http://java.sun.com/xml/ns/javaee\" "
-	        + "\n\t\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-	        + "\n\t\tversion=\"6\" "
-	        + "\n\t\txsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/application_6.xsd\">";
-
-	private static final String CONTEXT_ROOT_END = "</context-root>";
-
-	private static final String CONTEXT_ROOT_START = "<context-root>";
-
-	private static final String DISPLAY_NAME_END = "</display-name>";
-
-	private static final String DISPLAY_NAME_START = "<display-name>";
-
-	private static final String EJB_END = "</ejb>";
-	private static final String EJB_START = "<ejb>";
-	private static final String INDENT_1 = "\t";
-	private static final String INDENT_2 = "\t\t";
-	private static final String INDENT_3 = "\t\t\t";
-	private static final String INDENT_4 = "\t\t\t\t";
-	private static final String J2EE_VERSION_5 = "5";
-	private static final String J2EE_VERSION_6 = "6";
-	private static final String JAVA_END = "</java>";
-	private static final String JAVA_START = "<java>";
-	private static final String LIBRARY_DIRECTORY_END = "</library-directory>";
-	private static final String LIBRARY_DIRECTORY_START = "<library-directory>";
-	private static final String MODULE_END = "</module>";
-	private static final String MODULE_START = "<module>";
-	private static final String WEB_END = "</web>";
-	private static final String WEB_START = "<web>";
-	private static final String WEB_URI_END = "</web-uri>";
-	private static final String WEB_URI_START = "<web-uri>";
-	private static final String XML_HEADER = "<?xml version=\"1.0\" encoding=\"ASCII\"?>";
+	private static final String INDENT_1 = "\t"; //$NON-NLS-1$
+	private static final String INDENT_2 = "\t\t"; //$NON-NLS-1$
+	private static final String INDENT_3 = "\t\t\t"; //$NON-NLS-1$
+	private static final String INDENT_4 = "\t\t\t\t"; //$NON-NLS-1$
+	private static final String J2EE_VERSION_5 = "5"; //$NON-NLS-1$
+	private static final String J2EE_VERSION_6 = "6"; //$NON-NLS-1$
+	
 
 	private String appXmlFilename;
 	private String displayName;
@@ -88,67 +56,67 @@ public class AppXml extends Task {
 	public void execute() {
 		try {
 			if (appXmlFilename == null) {
-				throw new BuildException("AppXmlFilename not Set");
+				throw new BuildException(Messages.getString("AppXml.msg.filename.not.set")); //$NON-NLS-1$
 			}
-			log("Creating app xml " + appXmlFilename);
+			log("Creating app xml " + appXmlFilename); //$NON-NLS-1$
 			File output = new File(appXmlFilename);
 			PrintStream bus = new PrintStream(output);
 
-			bus.println(XML_HEADER);
+			bus.println(Messages.getString("AppXml.xml.header")); //$NON-NLS-1$
 
 			if (getVersion() == 6) {
-				bus.println(APPLICATION_START_J2EE_6_HEADER);
+				bus.println(Messages.getString("AppXml.tag.application.j2ee6")); // $NON-NLS-2$
 			} else {
-				bus.println(APPLICATION_START_J2EE_5_HEADER);
+				bus.println(Messages.getString("AppXml.tag.application.j2ee5")); // $NON-NLS-1$
 			}
 
-			bus.println(INDENT_1 + DISPLAY_NAME_START);
+			bus.println(INDENT_1 + Messages.getString("AppXml.tag.display.name")); //$NON-NLS-1$;
 			bus.print(INDENT_2);
 			bus.println(displayName);
-			bus.println(INDENT_1 + DISPLAY_NAME_END);
+			bus.println(INDENT_1 + Messages.getString("AppXml.tag.display.name.end")); //$NON-NLS-1$
 
 			for (Module fs : modules) {
 				fs.validate();
 
 				for (FileResource fileResource : fs.getFiles()) {
 					String name = fileResource.getFile().getName();
-					bus.println(INDENT_1 + MODULE_START);
+					bus.println(INDENT_1 + Messages.getString("AppXml.tag.module")); //$NON-NLS-1$;
 
 					if (fs.isWar()) {
-						bus.println(INDENT_2 + WEB_START);
-						bus.println(INDENT_3 + WEB_URI_START);
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.web")); //$NON-NLS-1$
+						bus.println(INDENT_3 + Messages.getString("AppXml.tag.web.uri")); //$NON-NLS-1$;
 						bus.print(INDENT_4);
 						bus.println(name);
-						bus.println(INDENT_3 + WEB_URI_END);
+						bus.println(INDENT_3 + Messages.getString("AppXml.tag.web.uri.end")); //$NON-NLS-1$
 
-						bus.println(INDENT_3 + CONTEXT_ROOT_START);
+						bus.println(INDENT_3 + Messages.getString("AppXml.tag.context.root")); //$NON-NLS-1$;
 						bus.print(INDENT_4);
 						bus.println(fs.getContextRoot());
-						bus.println(INDENT_3 + CONTEXT_ROOT_END);
-						bus.println(INDENT_2 + WEB_END);
+						bus.println(INDENT_3 + Messages.getString("AppXml.tag.context.root.end")); //$NON-NLS-1$
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.web.end")); //$NON-NLS-1$;
 					} else if (fs.isEjb()) {
-						bus.println(INDENT_2 + EJB_START);
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.ejb")); //$NON-NLS-1$;
 						bus.print(INDENT_3);
 						bus.println(name);
-						bus.println(INDENT_2 + EJB_END);
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.ejb.end")); // $NON-NLS-1$
 					} else {
-						bus.println(INDENT_2 + JAVA_START);
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.java")); //$NON-NLS-1$
 						bus.print(INDENT_3);
 						bus.println(name);
-						bus.println(INDENT_2 + JAVA_END);
+						bus.println(INDENT_2 + Messages.getString("AppXml.tag.java.end")); //$NON-NLS-1$
 					}
-					bus.println(INDENT_1 + MODULE_END);
+					bus.println(INDENT_1 + Messages.getString("AppXml.tag.module.end")); //$NON-NLS-1$;
 
 				}
 			}
 			if (libraryDirectory != null) {
-				bus.println(INDENT_1 + LIBRARY_DIRECTORY_START);
+				bus.println(INDENT_1 + Messages.getString("AppXml.tag.library.dir")); //$NON-NLS-1$
 				bus.print(INDENT_2);
 				bus.println(libraryDirectory);
-				bus.println(INDENT_1 + LIBRARY_DIRECTORY_END);
+				bus.println(INDENT_1 + Messages.getString("AppXml.tag.library.dir.end")); //$NON-NLS-1$
 
 			}
-			bus.println(APPLICATION_END);
+			bus.println(Messages.getString("AppXml.tag.application.end")); // $NON-NLS-1$
 			bus.close();
 
 		} catch (FileNotFoundException exception) {
